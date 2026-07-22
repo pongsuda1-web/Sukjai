3d1602773f9ff4892e5a6bd41ca4753ee8715995
Aeum <aeum@example.com>
2026-07-22 07:07:45 +0700

    Force hardcode supabase variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-let supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://aieqxcocarhxegzawovn.supabase.co';
-let supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_YkUPyYLNSekpVr1IB9PtCQ_iLdGqG1T';
-
-if (!supabaseUrl || !supabaseUrl.startsWith('http')) {
-  supabaseUrl = 'https://dummy.supabase.co';
-}
-if (!supabaseAnonKey) {
-  supabaseAnonKey = 'dummy_key';
-}
+let supabaseUrl = 'https://aieqxcocarhxegzawovn.supabase.co';
+let supabaseAnonKey = 'sb_publishable_YkUPyYLNSekpVr1IB9PtCQ_iLdGqG1T';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
